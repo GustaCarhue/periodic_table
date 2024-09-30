2e0a399b7418b991b6d980f183e86904366bf5a9
GustaCarhue <gggdupouy@gmail.com>
2024-09-30 22:19:10 +0000

    feat: actualizar mensaje de salida en element.sh

--- element.sh
+++ element.sh
@@ -4,7 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $1 ]]
 then
-  echo "Please provide an element as an argument."
+  echo "Por favor, proporciona un elemento como argumento."
 else
   ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1'")
   
