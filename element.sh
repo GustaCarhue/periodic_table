86de57cf33dc624235dc5bea2d22a136969979fa
GustaCarhue <gggdupouy@gmail.com>
2024-09-30 21:55:22 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1'")
+  
+  if [[ -z $ELEMENT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo "$ELEMENT"
+  fi
+fi
